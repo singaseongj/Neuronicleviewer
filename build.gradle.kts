1d10352ca663e49abb18d7a8be291a03c51306a0
singaseongapp <seongj1589@gmail.com>
2026-03-22 00:49:16 +0900

    modify repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:${libs.versions.agp.get()}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
+    }
 }
