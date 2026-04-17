ae4fa1cf89cc1245b97a4e0b5c3e4d3fb66061cf
singaseongapp <seongj1589@gmail.com>
2026-04-17 20:43:51 +0900

    modify repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.singaseongapp.neuronicleviewer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
